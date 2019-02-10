dee51f69b8f3fcbd9347f7805c8066db969fc313
Pierre Chat <pierrechat89@hotmail.fr>
2019-02-10 19:22:03 +0100

    Adding a new script command

--- polydrive/config/scripts.py
+++ polydrive/config/scripts.py
@@ -3,8 +3,13 @@ from polydrive.services.database import init_db, clear_db, fill_db
 
 
 @manager.command
-def initdb():
+def init_fake_db():
     clear_db()
     init_db()
     fill_db('../data/data.yml')
-    print('Database initialized')
+
+
+@manager.command
+def init_db():
+    clear_db()
+    init_db()
