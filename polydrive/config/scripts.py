fc2a2b4f5412f0bada2cf0a2b34a175a11157e2e
Pierre Chat <pierrechat89@hotmail.fr>
2019-02-10 19:47:45 +0100

    Fix

--- polydrive/config/scripts.py
+++ polydrive/config/scripts.py
@@ -3,13 +3,13 @@ from polydrive.services.database import init_db, clear_db, fill_db
 
 
 @manager.command
-def init_fake_db():
+def init_fake_database():
     clear_db()
     init_db()
     fill_db('../data/data.yml')
 
 
 @manager.command
-def init_db():
+def init_database():
     clear_db()
     init_db()
